bb7a37764f4688dd6046188b14989a37ce952fec
“mawar” <jonathanfrizy12@gmail.com>
2025-10-23 09:30:43 +0700

    feat/calculator | JonathanLim46: add components to form calculator

---
+++ src/features/calculator/components/InputNumberGroup.jsx
@@ -0,0 +1,21 @@
+import { Field, Input, Label } from '@headlessui/react';
+
+function InputNumber({ title, name, value, onChange }) {
+    return (
+        <>
+            <Field>
+                <Label>{title}</Label>
+                <Input
+                    type='number'
+                    name={name}
+                    min={0}
+                    value={value}
+                    onChange={onChange}
+                    className="bg-gray-100 rounded w-11/12 h-9 focus:outline-gray-200 px-4"
+                />
+            </Field>
+        </>
+    )
+}
+
+export default InputNumber;
