a9a8343ecf4d3611b34d42a6ff0b131c59d3369a
Alvinda_akbar10 <89963240+Nandaakbar15@users.noreply.github.com>
2025-10-24 22:51:25 +0700

    feat(backend): add ROI calculation API and Prisma integration

--- server/app.js
+++ server/app.js
@@ -1,1 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
+import express from "express";
+import cors from "cors";
+import router from "../server/routes/route.js";
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(router);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    statusCode: 200,
+    message: "ROI Calculator App",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at: http://localhost:${port}`);
+});
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,1 +1,12 @@
- 
+import express from "express";
+import {
+  getAllRoiResults,
+  calculateRoi,
+} from "../controllers/CalculatorController.js";
+
+const router = express.Router();
+
+router.get("/api/roi-results/", getAllRoiResults);
+router.post("/api/calculate-roi", calculateRoi);
+
+export default router;
