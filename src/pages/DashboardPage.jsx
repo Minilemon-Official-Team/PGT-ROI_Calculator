a71c3c5e77dc15ab58ea846bfe715945e89ec5c1
“mawar” <jonathanfrizy12@gmail.com>
2025-10-31 20:29:49 +0700

    feat/dashboard | JonathanLim46: add handler context for dashboard page

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -3,7 +3,18 @@ import DashboardHeader from "../features/dashboard/components/DashboardHeader";
 import TabNavigations from "../features/dashboard/components/TabNavigations";
 import { Target, DollarSign, Calendar } from "lucide-react";
 
+import { useContext } from 'react';
+import { NavStateContext } from '../shared/contexts';
+import { Navigate } from "react-router-dom";
+
 function DashboardPage() {
+
+    const navState = useContext(NavStateContext);
+
+    if (!navState.formCalculated) {
+        return <Navigate to={"/kalkulator"} replace />
+    }
+
     return (
         <>
             <section className="flex flex-col w-sm md:w-6xl m-auto py-8">
